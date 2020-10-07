352907e600eab5141529f190057cbc34ffbea4d7
EndlessVoid <50932323+EndlessVoid27@users.noreply.github.com>
2020-10-06 19:06:43 -0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+function typewrite() { 
+    var curPos =  
+        document.getElementById("text1").selectionStart; 
+    console.log(curPos); 
+    let x = $("#text1").val(); 
+    let text_to_insert = $("#insert").val(); 
+    $("#text1").val( 
+x.slice(0, curPos) + text_to_insert + x.slice(curPos)); 
+} 
