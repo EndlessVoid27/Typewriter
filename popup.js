9444f28c8396a2fe997ddd5c43c61dded19343f1
ReactivePeakStudios <52389664+ReactivePeakStudios@users.noreply.github.com>
2020-10-06 19:08:26 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
 function typewrite() { 
     var curPos =  
-        document.getElementById("text1").selectionStart; 
-    console.log(curPos); 
+        document.getElementById("text1").selectionStart;
     let x = $("#text1").val(); 
     let text_to_insert = $("#insert").val(); 
     $("#text1").val( 
 x.slice(0, curPos) + text_to_insert + x.slice(curPos)); 
-} 
+}
